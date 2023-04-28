3942e602ecb862106179ca55fd1d2f0be5dae181
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-04-28 13:51:23 +0900

    00_05_課題途中

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ static const int kCHei = 20;
 // 回転の順序で答えが違う
 //RoteteOrder
 
-
+/*
 
 Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 	Matrix4x4 result;
@@ -46,7 +46,7 @@ Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 }
 
 
-
+*/
 Matrix4x4 MakeRotateXMatrix(float radian)
 {
 	Matrix4x4 result;
@@ -99,6 +99,7 @@ Matrix4x4 MakeRotateYMatrix(float radian)
 	result.m[3][2] = 0.0f;
 	result.m[3][3] = 1.0f;
 
+
 	return result;
 }
 
@@ -138,6 +139,22 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4 matrix, const char* label)
 	}
 }
 
+Vector3 Multiply(float scalar, const Vector3& v)
+{
+	Vector3 result{};
+	result.x = scalar * v.x;
+	result.y = scalar * v.y;
+	result.z = scalar * v.z;
+	return result;
+}
+
+
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
+{
+
+
+}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -149,14 +166,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-
+	Vector3 scale = { 1.2f,0.79f,-2.1f };
 	Vector3 rotate = { 0.4f,1.43f,-0.8f };
+	Vector3 translate = { 2.7f,-4.15f,1.57f };
+	Matrix4x4 worldMatrix;
 
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
-	// ウィンドウの×ボタンが押されるまでループ
+
+// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
 		Novice::BeginFrame();
@@ -177,12 +193,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
-		MatrixScreenPrintf(0, kCHei * 5, rotateYMatrix, "rotateYMatrix");
-		MatrixScreenPrintf(0, kCHei * 5 *2 , rotateZMatrix, "rotateZMatrix");
-		MatrixScreenPrintf(0, kCHei * 5 *3, rotateXYZMatrix, "rotateXYZMatrix");
-
-
+	
 		
 		/// ↑描画処理ここまで
 		///
